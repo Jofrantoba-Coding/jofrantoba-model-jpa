1867fedd6af0dcc9efd87179d232873993cbfadb
jofrantoba <chescot2302@gmail.com>
2023-03-16 12:16:43 -0500

    stack trace dev

--- src/main/java/com/jofrantoba/model/jpa/shared/UnknownException.java
+++ src/main/java/com/jofrantoba/model/jpa/shared/UnknownException.java
@@ -1,6 +1,8 @@
 package com.jofrantoba.model.jpa.shared;
 
+import java.io.PrintWriter;
 import java.io.Serializable;
+import java.io.StringWriter;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
@@ -28,26 +30,13 @@ public class UnknownException extends Exception implements Serializable {
                         boolean writableStackTrace){
         super(message, cause, enableSuppression, writableStackTrace);
         this.clase=clase;
-    }
-
+    }    
     public void traceLog(boolean allTrace){        
-        StringBuilder logTrace=new StringBuilder();
-        logTrace.append("\n");
-        logTrace.append("***Inicia UnknownException***").append("\n");
-        logTrace.append(".:.:.stacktrace.:.:.").append("\n");        
-        logTrace.append("Mensaje:").append(this.getMessage()).append("\n");
-        logTrace.append("Causa:").append(this.getCause()).append("\n");
-        logTrace.append("Clase:").append(this.getClase()).append("\n");
-        logTrace.append("Localizacion:").append(this.getLocalizedMessage()).append("\n");        
-        for(int i=0;i<this.getStackTrace().length;i++){                       
-            if(!allTrace && i>0){
-                continue;
-            }
-            logTrace.append("En ").append(this.getStackTrace()[i].getClassName()).append(".").append(this.getStackTrace()[i].getMethodName()).append("(").append(this.getStackTrace()[i].getFileName()).append(":").append(this.getStackTrace()[i].getLineNumber()).append(")\n");                       
-        }
-        logTrace.append(".:.:.stacktrace.:.:.").append("\n");        
-        logTrace.append("***Termina UnknownException***").append("\n");
-        log.error(logTrace);
+        StringWriter stack = new StringWriter();
+        super.printStackTrace(new PrintWriter(stack));
+        if (allTrace) {
+            log.error(stack.toString());
+        }        
     }
     
 }
